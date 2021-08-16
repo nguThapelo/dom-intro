06a13b113312f670c26136bd9bf4b1cc5b4738b1
Thapelo Mmakola <mthapelothapza@gmail.com>
2021-08-16 14:15:25 +0200

    changes

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,59 +1,60 @@
 //get a reference to the calculate button
 const calculateBtn = document.querySelector(".calculateBtn");
+
 //get a reference to the billTotal element
-const billTotalElement = document.querySelector(".billTotal");
+const billTotals = document.querySelector(".billTotal");
+
 //get a reference to the billString
-const billStringElement = document.querySelector(".billString");
-//get a reference to the Total
-const billTotalSpanElement = document.querySelector(".Total");
+const billStringX = document.querySelector(".billString");
+
+//get a reference to the billString
+const billTotalE = document.querySelector(".total");
 
 //create the function that will be called when the calculate button is pressed
-function calculateBtnClicked() {
+function calculateBtnPressed() {
 
 //  * this function should read the string value entered - split it on a comma.
-  var billString = billStringElement.value;
-  var billItems = billString.split(",");
-  var billTotal = 0;
+var billString = billStringX.value;
+var billE = billString.split(",");
+var billTotal = 0;
 
 //  * loop over all the entries in the the resulting list
-for (var i = 0; i < billItems.length; i++) {
+for (var i = 0; i < billE.length; i++) {
 
-  //trim the spaces that might occur
-    var billItem = billItems[i].trim();
+var bills = billE[i].trim();
 
 //  * check if it is a call or an sms and add the right amount to the overall total
-if (billItem.includes("c")) {
-      billTotal += 2.75;
 
-} else if (billItem.includes("s")) {
-      billTotal += 0.75;
-    }
-  }
+if(bills.includes("c")) {
+  billTotal += 2.75;
+} else if (bills.includes("s")) {
+  billTotal += 0.75;
+}
+}
 
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
-  var roundedBillTotal = billTotal.toFixed(2);
+var roundBillTotal = billTotal.toFixed(2);
 
-  //link the function to a click event on the calculate button
-  function styleTotalColor(roundedBillTotal) {
+//link the function to a click event on the calculate button
+function styleRoundedBillTotal(roundBillTotal) {
 
-    const currentTotal = Number(roundedBillTotal);
+  var Total = Number(roundBillTotal);
 
-    //remove class
-    billTotalSpanElement.classList.remove("danger");
-    billTotalSpanElement.classList.remove("warning");
+    billTotalE.classList.remove("danger");
+    billTotalE.classList.remove("warning");
 
-        // to display red when cost if greater or equal 30
-    if (currentTotal >= 30) {
-      billTotalSpanElement.classList.add("danger");
-    } 
-        // to display orange when cost if greater or equal 20 and less than 30
-    else if (currentTotal >= 20 && currentTotal < 30) {
-      billTotalSpanElement.classList.add("warning");
-    }
+  if(Total > 30) {
+      billTotalE.classList.add("danger");
+  } else if (Total >= 20 && Total < 30) {
+    billTotalE.classList.add("warning");
+
   }
-  billTotalElement.innerHTML = roundedBillTotal;
-  styleTotalColor(roundedBillTotal);
+  
 }
-//event listener 
-calculateBtn.addEventListener("click", calculateBtnClicked);
+billTotals.innerHTML = roundBillTotal;
+  styleRoundedBillTotal(roundBillTotal);
+}
+
+calculateBtn.addEventListener("click", calculateBtnPressed);
+
 
